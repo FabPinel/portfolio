87a94694d73092b4c2cd73bfcdd6a5a829d421dc
Fabien Pinel <fabien.pinel08@gmail.com>
2024-03-13 18:54:11 +0100

    Adding NavBar + redirection + fix background section

--- resources/js/Components/Section/SkillsSection.tsx
+++ resources/js/Components/Section/SkillsSection.tsx
@@ -1,144 +1,146 @@
+import { useRef } from "react";
+
 interface SkillsProps {
     darkMode: boolean;
 }
 
 export default function SkillsSection({ darkMode }: SkillsProps) {
+    const skillsRef = useRef<HTMLDivElement>(null);
+
     return (
-        <div className={`${darkMode ? "bg-bgdark" : "bg-bglight"}`}>
-            <div className="mx-auto max-w-7xl">
-                <div
-                    className={`${
-                        darkMode ? "text-txtdark" : "text-txtlight"
-                    } mx-auto text-center font-semibold text-4xl`}
-                >
-                    <h2>Skills</h2>
+        <div id="skills" className="mx-auto max-w-7xl mt-20">
+            <div
+                className={`${
+                    darkMode ? "text-txtdark" : "text-txtlight"
+                } mx-auto text-center font-semibold text-4xl`}
+            >
+                <h2>Skills</h2>
+            </div>
+            <div className="w-full mt-10 grid grid-cols-5 justify-center items-center">
+                <div>
+                    <h3
+                        className={`${
+                            darkMode ? "text-txtdark" : "text-txtlight"
+                        } mt-6 text-xl font-bold mb-5`}
+                    >
+                        Design
+                    </h3>
+                    <p
+                        className={`${
+                            darkMode
+                                ? "bg-gradient-to-t from-[#079ABB] to-[#44D7F8] bg-clip-text text-transparent"
+                                : "bg-gradient-to-t from-[#079ABB] to-[#B7EBF0] bg-clip-text text-transparent"
+                        } mt-2 text-lg font-bold flex items-center`}
+                    >
+                        photoshop
+                    </p>
+                    <p
+                        className={`${
+                            darkMode ? "text-txtdark" : "text-txtlight"
+                        } mt-2 text-lg font-semibold`}
+                    >
+                        figma
+                    </p>
                 </div>
-                <div className="w-full mt-10 grid grid-cols-5 justify-center items-center">
-                    <div>
-                        <h3
-                            className={`${
-                                darkMode ? "text-txtdark" : "text-txtlight"
-                            } mt-6 text-xl font-bold mb-5`}
-                        >
-                            Design
-                        </h3>
-                        <p
-                            className={`${
-                                darkMode
-                                    ? "bg-gradient-to-t from-[#079ABB] to-[#44D7F8] bg-clip-text text-transparent"
-                                    : "bg-gradient-to-t from-[#079ABB] to-[#B7EBF0] bg-clip-text text-transparent"
-                            } mt-2 text-lg font-bold flex items-center`}
-                        >
-                            photoshop
-                        </p>
-                        <p
-                            className={`${
-                                darkMode ? "text-txtdark" : "text-txtlight"
-                            } mt-2 text-lg font-semibold`}
-                        >
-                            figma
-                        </p>
-                    </div>
-                    <div>
-                        <h3
-                            className={`${
-                                darkMode ? "text-txtdark" : "text-txtlight"
-                            } mt-6 text-xl font-bold mb-5`}
-                        >
-                            Frontend
-                        </h3>
-                        <p
-                            className={`${
-                                darkMode ? "text-txtdark" : "text-txtlight"
-                            } mt-2 text-lg font-semibold`}
-                        >
-                            react
-                        </p>
-                        <p
-                            className={`${
-                                darkMode
-                                    ? "bg-gradient-to-t from-[#079ABB] to-[#44D7F8] bg-clip-text text-transparent"
-                                    : "bg-gradient-to-t from-[#079ABB] to-[#B7EBF0] bg-clip-text text-transparent"
-                            } mt-2 text-lg font-bold`}
-                        >
-                            tailwind
-                        </p>
-                    </div>
-                    <div>
-                        <h3
-                            className={`${
-                                darkMode ? "text-txtdark" : "text-txtlight"
-                            } mt-6 text-xl font-bold mb-5`}
-                        >
-                            Backend
-                        </h3>
-                        <p
-                            className={`${
-                                darkMode ? "text-txtdark" : "text-txtlight"
-                            } mt-2 text-lg font-semibold`}
-                        >
-                            laravel
-                        </p>
-                        <p
-                            className={`${
-                                darkMode
-                                    ? "bg-gradient-to-t from-[#079ABB] to-[#44D7F8] bg-clip-text text-transparent"
-                                    : "bg-gradient-to-t from-[#079ABB] to-[#B7EBF0] bg-clip-text text-transparent"
-                            } mt-2 text-lg font-bold`}
-                        >
-                            mysql
-                        </p>
-                    </div>
-                    <div>
-                        <h3
-                            className={`${
-                                darkMode ? "text-txtdark" : "text-txtlight"
-                            } mt-6 text-xl font-bold mb-5`}
-                        >
-                            Management
-                        </h3>
-                        <p
-                            className={`${
-                                darkMode
-                                    ? "bg-gradient-to-t from-[#079ABB] to-[#44D7F8] bg-clip-text text-transparent"
-                                    : "bg-gradient-to-t from-[#079ABB] to-[#B7EBF0] bg-clip-text text-transparent"
-                            } mt-2 text-lg font-bold`}
-                        >
-                            planning
-                        </p>
-                        <p
-                            className={`${
-                                darkMode ? "text-txtdark" : "text-txtlight"
-                            } mt-2 text-lg font-semibold`}
-                        >
-                            specification
-                        </p>
-                    </div>
-                    <div>
-                        <h3
-                            className={`${
-                                darkMode ? "text-txtdark" : "text-txtlight"
-                            } mt-6 text-xl font-bold mb-5`}
-                        >
-                            Other
-                        </h3>
-                        <p
-                            className={`${
-                                darkMode
-                                    ? "bg-gradient-to-t from-[#079ABB] to-[#44D7F8] bg-clip-text text-transparent"
-                                    : "bg-gradient-to-t from-[#079ABB] to-[#B7EBF0] bg-clip-text text-transparent"
-                            } mt-2 text-lg font-bold`}
-                        >
-                            git & github
-                        </p>
-                        <p
-                            className={`${
-                                darkMode ? "text-txtdark" : "text-txtlight"
-                            } mt-2 text-lg font-semibold`}
-                        >
-                            postman
-                        </p>
-                    </div>
+                <div>
+                    <h3
+                        className={`${
+                            darkMode ? "text-txtdark" : "text-txtlight"
+                        } mt-6 text-xl font-bold mb-5`}
+                    >
+                        Frontend
+                    </h3>
+                    <p
+                        className={`${
+                            darkMode ? "text-txtdark" : "text-txtlight"
+                        } mt-2 text-lg font-semibold`}
+                    >
+                        react
+                    </p>
+                    <p
+                        className={`${
+                            darkMode
+                                ? "bg-gradient-to-t from-[#079ABB] to-[#44D7F8] bg-clip-text text-transparent"
+                                : "bg-gradient-to-t from-[#079ABB] to-[#B7EBF0] bg-clip-text text-transparent"
+                        } mt-2 text-lg font-bold`}
+                    >
+                        tailwind
+                    </p>
+                </div>
+                <div>
+                    <h3
+                        className={`${
+                            darkMode ? "text-txtdark" : "text-txtlight"
+                        } mt-6 text-xl font-bold mb-5`}
+                    >
+                        Backend
+                    </h3>
+                    <p
+                        className={`${
+                            darkMode ? "text-txtdark" : "text-txtlight"
+                        } mt-2 text-lg font-semibold`}
+                    >
+                        laravel
+                    </p>
+                    <p
+                        className={`${
+                            darkMode
+                                ? "bg-gradient-to-t from-[#079ABB] to-[#44D7F8] bg-clip-text text-transparent"
+                                : "bg-gradient-to-t from-[#079ABB] to-[#B7EBF0] bg-clip-text text-transparent"
+                        } mt-2 text-lg font-bold`}
+                    >
+                        mysql
+                    </p>
+                </div>
+                <div>
+                    <h3
+                        className={`${
+                            darkMode ? "text-txtdark" : "text-txtlight"
+                        } mt-6 text-xl font-bold mb-5`}
+                    >
+                        Management
+                    </h3>
+                    <p
+                        className={`${
+                            darkMode
+                                ? "bg-gradient-to-t from-[#079ABB] to-[#44D7F8] bg-clip-text text-transparent"
+                                : "bg-gradient-to-t from-[#079ABB] to-[#B7EBF0] bg-clip-text text-transparent"
+                        } mt-2 text-lg font-bold`}
+                    >
+                        planning
+                    </p>
+                    <p
+                        className={`${
+                            darkMode ? "text-txtdark" : "text-txtlight"
+                        } mt-2 text-lg font-semibold`}
+                    >
+                        specification
+                    </p>
+                </div>
+                <div>
+                    <h3
+                        className={`${
+                            darkMode ? "text-txtdark" : "text-txtlight"
+                        } mt-6 text-xl font-bold mb-5`}
+                    >
+                        Other
+                    </h3>
+                    <p
+                        className={`${
+                            darkMode
+                                ? "bg-gradient-to-t from-[#079ABB] to-[#44D7F8] bg-clip-text text-transparent"
+                                : "bg-gradient-to-t from-[#079ABB] to-[#B7EBF0] bg-clip-text text-transparent"
+                        } mt-2 text-lg font-bold`}
+                    >
+                        git & github
+                    </p>
+                    <p
+                        className={`${
+                            darkMode ? "text-txtdark" : "text-txtlight"
+                        } mt-2 text-lg font-semibold`}
+                    >
+                        postman
+                    </p>
                 </div>
             </div>
         </div>
--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -7,6 +7,7 @@ import SkillsSection from "@/Components/Section/SkillsSection";
 import ContactSection from "@/Components/Section/ContactSection";
 import EducationSection from "@/Components/Section/EducationSection";
 import ProjectsSection from "@/Components/Section/ProjectsSection";
+import NavBar from "@/Components/NavBar";
 
 export default function Welcome({
     auth,
@@ -15,50 +16,15 @@ export default function Welcome({
 
     return (
         <>
-            <Head title="Welcome" />
-            <div
-                className={`${
-                    darkMode ? "bg-bgdark" : "bg-bglight"
-                } relative sm:flex sm:justify-center sm:items-center min-h-screen`}
-            >
-                <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-end flex">
-                    {auth.user ? (
-                        <Link
-                            href={route("dashboard")}
-                            className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                        >
-                            Dashboard
-                        </Link>
-                    ) : (
-                        <>
-                            <Link
-                                href={route("login")}
-                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                            >
-                                Log in
-                            </Link>
-
-                            <Link
-                                href={route("register")}
-                                className="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                            >
-                                Register
-                            </Link>
-                        </>
-                    )}
-                    <div className="ml-4">
-                        <SwitchMode
-                            setDarkMode={setDarkMode}
-                            darkMode={darkMode}
-                        />
-                    </div>
-                </div>
+            <div className={`${darkMode ? "bg-bgdark" : "bg-bglight"} `}>
+                <NavBar setDarkMode={setDarkMode} darkMode={darkMode}></NavBar>
+                <Head title="Welcome" />
                 <HeroSection darkMode={darkMode}></HeroSection>
+                <SkillsSection darkMode={darkMode}></SkillsSection>
+                <ProjectsSection darkMode={darkMode}></ProjectsSection>
+                <EducationSection darkMode={darkMode}></EducationSection>
+                <ContactSection darkMode={darkMode}></ContactSection>
             </div>
-            <SkillsSection darkMode={darkMode}></SkillsSection>
-            <ProjectsSection darkMode={darkMode}></ProjectsSection>
-            <EducationSection darkMode={darkMode}></EducationSection>
-            <ContactSection darkMode={darkMode}></ContactSection>
         </>
     );
 }
