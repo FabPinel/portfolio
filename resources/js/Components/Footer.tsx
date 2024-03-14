7c715829eb85b8c5d786e105736c09abd1d9a9e8
Fabien Pinel <fabien.pinel08@gmail.com>
2024-03-14 13:51:17 +0100

    Adding footer

---
+++ resources/js/Components/Footer.tsx
@@ -0,0 +1,22 @@
+interface FooterProps {
+    darkMode: boolean;
+}
+
+export default function Footer({ darkMode }: FooterProps) {
+    return (
+        <footer className="rounded-lg">
+            <div className="w-full p-4 text-center">
+                <span className="text-sm text-gray-500 sm:text-center">
+                    © 2024{" "}
+                    <a
+                        href="https://www.linkedin.com/in/fabien-pinel/"
+                        className="hover:underline"
+                    >
+                        Fabien Pinel
+                    </a>
+                    . All Rights Reserved.
+                </span>
+            </div>
+        </footer>
+    );
+}
