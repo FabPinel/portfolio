4863882fecb28ae135e946f909dadbd7f9423332
Fabien Pinel <fabien.pinel08@gmail.com>
2024-03-14 14:59:32 +0100

    Fix css hero section

--- resources/js/Components/Section/HeroSection.tsx
+++ resources/js/Components/Section/HeroSection.tsx
@@ -13,11 +13,11 @@ export default function HeroSection({ darkMode }: HeroProps) {
                 darkMode ? "bg-bgdark" : "bg-bglight"
             } relative isolate overflow-hidden`}
         >
-            <div className="mx-auto max-w-7xl pb-24 pt-10 sm:pb-32 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-40">
-                <div className="px-6 lg:px-0 lg:pt-4">
+            <div className="mx-auto max-w-7xl flex flex-wrap justify-center py-0 md:py-40 lg:md:py-40">
+                <div className="px-6 lg:px-0">
                     <div className="mx-auto max-w-2xl">
                         <div className="max-w-lg">
-                            <div className="mt-24 sm:mt-32 lg:mt-16">
+                            <div className="mt-2 sm:mt-32 lg:mt-16">
                                 <span
                                     className={`${
                                         darkMode
